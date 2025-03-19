b8b7df70358e0ad0c59b68f59c369d21a78ee2fa
Carlos Verde <cverde22@gmail.com>
2025-03-19 16:43:05 +0100

    Start commit with base architecture and dependencies

---
+++ src/main/java/com/inditex/peoplecore/PeopleCoreApplication.java
@@ -0,0 +1,13 @@
+package com.inditex.peoplecore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleCoreApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PeopleCoreApplication.class, args);
+  }
+
+}
